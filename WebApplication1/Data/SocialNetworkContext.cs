ddc497103effee1940c78c73ccc6f630ad4458db
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-09-08 18:02:51 -0300

    Add project files.

---
+++ WebApplication1/Data/SocialNetworkContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data
+{
+    public class SocialNetworkContext : DbContext
+    {
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=tcp:amazingsimplesocialnetwork-db-server.database.windows.net,1433;Initial Catalog=AmazingSimpleSocialNetwork-Db;Persist Security Info=False;User ID=gustavo;Password=@dsInf123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        }
+    }
+}
