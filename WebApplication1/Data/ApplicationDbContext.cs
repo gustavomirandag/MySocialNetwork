d75a2f80442bea16e1fb23abf6bf2172491296e9
Gustavo de Miranda <gustavolpu@hotmail.com>
2020-09-08 18:03:24 -0300

    MySocialNetwork

--- WebApplication1/Data/ApplicationDbContext.cs
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace WebApplication1.Data
     {
         public ApplicationDbContext()
         {
+
         }
 
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
